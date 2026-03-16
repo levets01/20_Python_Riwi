6041943875879b3783879ac23f919fc2a13b4e65
ERROR_404 <68274153+levets01@users.noreply.github.com>
2026-03-16 12:51:45 -0500

    Create cine.py

---
+++ cine.py
@@ -0,0 +1,17 @@
+precioa = 8000
+precion = 12000
+precioA = 9000
+
+
+    
+cantidad_adultos = int(input("CUANTAS PERSONAS DESEAN INGRESAR : "))
+edad_adultos = int(input("CUANTAS PERSONAS DESEAN INGRESAR : "))
+    
+    
+if edad_adultos < 12 and 59 :
+        total_adultos = cantidad_adultos * precioa 
+        
+        print (f"EL VALOR A PAGAR ES ", {precioa})
+        
+
+
